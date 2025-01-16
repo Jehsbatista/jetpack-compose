eb05eff9c8f0487fa3e06caa67c379af24adefdc
jessicabatista <jessica.batista@g.globo>
2025-01-16 16:14:04 -0300

    crindo novo elemento e i serindo uma imagem a ele

--- Compose/app/src/main/java/com/example/basiccodelabcompose/lessons/SecondLesson.kt
+++ Compose/app/src/main/java/com/example/basiccodelabcompose/lessons/SecondLesson.kt
@@ -1,8 +1,12 @@
 package com.example.basiccodelabcompose.lessons
 
 import BasicsCodelabTheme
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Icon
@@ -12,6 +16,9 @@ import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -42,6 +49,35 @@ fun SearchBar(
             .heightIn(min = 56.dp)
     )
 }
+
+@Composable
+fun AlignYourBodyElement(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+    ) {
+        Image(
+            painter = painterResource(R.drawable.picture),
+            contentDescription = null,
+            modifier = Modifier
+                .size(88.dp)
+                .clip(CircleShape),
+            contentScale = ContentScale.Crop
+        )
+        Text(text = stringResource(R.string.inversions))
+    }
+}
+
+
+@Preview
+@Composable
+fun AlignYourBodyElementPreview() {
+    BasicsCodelabTheme {
+        AlignYourBodyElement()
+    }
+}
+
 
 @Preview
 @Composable
