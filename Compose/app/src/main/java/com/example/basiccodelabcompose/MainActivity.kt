7151ce96af98caad01aff1c68e4f7fec4e12b2b2
jessicabatista <jessica.batista@g.globo>
2025-01-15 21:08:17 -0300

    criando botão e Classe para segunda lição

--- Compose/app/src/main/java/com/example/basiccodelabcompose/MainActivity.kt
+++ Compose/app/src/main/java/com/example/basiccodelabcompose/MainActivity.kt
@@ -4,9 +4,11 @@ import BasicsCodelabTheme
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
@@ -17,12 +19,16 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.basiccodelabcompose.lessons.FirstLesson
+import com.example.basiccodelabcompose.lessons.SecondLesson
 
 const val LESSON_ONE = "Lição 1"
+const val LESSON_TWO = "Lição 2"
 const val SAMPLE = "Sample"
 
 class MainActivity : ComponentActivity() {
@@ -44,8 +50,15 @@ fun MyApp(modifier: Modifier = Modifier) {
 
         when (state) {
             LESSON_ONE -> FirstLesson()
+            LESSON_TWO -> SecondLesson()
             else -> {
-                Column {
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp),
+                    verticalArrangement = Arrangement.spacedBy(5.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
                     Button(
                         modifier = Modifier
                             .fillMaxWidth()
@@ -53,6 +66,14 @@ fun MyApp(modifier: Modifier = Modifier) {
                         onClick = { state = LESSON_ONE }
                     ) {
                         Text(text = LESSON_ONE, fontSize = 24.sp)
+                    }
+                    Button(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .wrapContentHeight(),
+                        onClick = { state = LESSON_TWO }
+                    ) {
+                        Text(text = LESSON_TWO, fontSize = 24.sp)
                     }
                 }
             }
